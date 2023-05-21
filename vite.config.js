a01ae0bc921a7fa19825485b9907e4211fafbe86
Bogdan <bogdangaponenko1996@gmail.com>
2023-05-21 00:12:08 -0500

    FIX: trying to fix problem with not showing app in the Giyhub

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
-  base: '/',
+  base: '/project-tweets/',
   plugins: [react(), eslint()],
   server: { open: '/index.html' },
 });
